c0d58f8315682a98d39d8dc25915f46e01570b94
Yasen Atanasov <yasen.atanasov@gmail.com>
2015-07-25 13:44:14 +0300

    this was causing problems in genymotion, too lazy to find out why

--- src/com/tinyrssreader/activities/actionbar/TinyRSSReaderActivity.java
+++ src/com/tinyrssreader/activities/actionbar/TinyRSSReaderActivity.java
@@ -51,8 +51,8 @@ public abstract class TinyRSSReaderActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(getLayout());
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-			getActionBar().setHomeButtonEnabled(true);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+//			getActionBar().setHomeButtonEnabled(true);
 		}
 		initialize();
 	}
